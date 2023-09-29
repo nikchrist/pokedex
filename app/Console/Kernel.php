fe3841e8f8eb548023d8bce1e30c13f64dde1d20
nikchrist <nikoschristomanos@gmail.com>
2023-09-29 05:40:09 +0300

    Scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Controllers\PokemonsController;
+use App\Models\Pokemons;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $pokemons = new PokemonsController;
+        $schedule->call($pokemons->updatePokemons())->sundays()->at('9:00');
         // $schedule->command('inspire')->hourly();
     }
 
--- app/Http/Controllers/PokemonsController.php
+++ app/Http/Controllers/PokemonsController.php
@@ -125,4 +125,93 @@ class PokemonsController extends Controller
                 
     }
 
+    public function updatePokemons(){
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        DB::table('pokemon_categories_pokemons')->truncate();
+        DB::table('pokemon_categories')->truncate();
+        DB::table('pokemons')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        $allcategories = Http::async()->get("https://pokeapi.co/api/v2/type")
+                ->then(function($response){
+                    return $response->object()->results;
+                })->wait();
+                foreach($allcategories as $category)
+                {
+                    $pokemoncat = new PokemonCategories;
+                    $pokemoncat->name = $category->name;
+                    $pokemoncat->save();
+                }
+        $limit = -1;
+        $offset = 0;
+        $mainurl = $this->endpoint.'?limit='.$limit.'&offset='.$offset;
+        /*Get all pokemons  */
+        Http::async()->get($mainurl)
+        ->then(function($response){
+            return $response->object()->results;
+        })->then(function($response){
+            foreach($response as $pokemon_name_obj)
+            {
+                $name  = $pokemon_name_obj->name;
+                $pokemoninfo = Http::pool(function(Pool $pool)use($name){
+                    return $pool->get("https://pokeapi.co/api/v2/pokemon/".$name);
+                });
+                foreach($pokemoninfo as $singlepokemon)
+                {
+                    $pokemoninfo = [];
+                    $pokemon = new Pokemons;
+                    $pokemonobj = $singlepokemon->object();
+                    
+                    $pokemon->name = $pokemonobj->name;
+                    $pokemon->weight = $pokemonobj->weight;
+                    $pokemon->height = $pokemonobj->height;
+                    $pokemon->pokemonid = $pokemonobj->id;
+                    $pokemon_stats = $pokemonobj->stats;
+                    $pokemon_types = $pokemonobj->types;
+                    $pokemon->image = $pokemonobj->sprites->front_shiny;
+                    foreach($pokemon_stats as $stat)
+                    {
+                        switch($stat->stat->name)
+                        {
+                            case("hp"):
+                                $pokemon->hp= $stat->base_stat;
+                            break;
+                            case("special-defense"):
+                                $pokemon->special_defence = $stat->base_stat;
+                            break;
+                            case("special-attack"):
+                                $pokemon->special_attack = $stat->base_stat;
+                            break;
+                            case("attack"):
+                                $pokemon->attack = $stat->base_stat;
+                            break;
+                            case("defense"):
+                                $pokemon->defence = $stat->base_stat;
+                            break;
+                            case("speed"):
+                                $pokemon->speed = $stat->base_stat;
+                            break;
+                        }
+                    }
+                    $pokemon->save();
+                    $this->pokemoncatnames = PokemonCategories::select('id','name')->get();
+                    foreach($pokemon_types as $type)
+                    {
+                        foreach($this->pokemoncatnames->toArray() as $existingcatname)
+                        {
+                            if($existingcatname["name"] == $type->type->name)
+                            {
+                                DB::table('pokemon_categories_pokemons')->insert(
+                                    [
+                                        "pokemon_categories_id" => $existingcatname["id"],
+                                        "pokemons_id" => $pokemon->id
+                                    ]
+                                );
+                            }
+                        }
+                    }
+                }
+        }
+        })->wait();
+    }
+
 }
